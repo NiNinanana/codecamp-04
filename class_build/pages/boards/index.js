e77d4a421dd7f737cc140addef7c21da6da9065b
NiNinanana <yeen00123@gmail.com>
2021-12-21 11:16:45 +0900

    1221-1 로그인 문제 해결

---
+++ class_build/pages/boards/index.js
@@ -0,0 +1,9 @@
+import Head from "next/head";
+
+export default function BoardsPage() {
+  return (
+    <>
+      <div>안녕하세요 게시판입니당</div>
+    </>
+  );
+}
--- freeboard_frontend/pages/_app.tsx
+++ freeboard_frontend/pages/_app.tsx
@@ -22,22 +22,6 @@ import {
   useEffect,
 } from "react";
 import { getAccessToken } from "../src/commons/libraries/getAccessToken";
-// // Set the configuration for your app
-// // TODO: Replace with your project's config object
-// const firebaseConfig = {
-//   apiKey: "apiKey",
-//   authDomain: "projectId.firebaseapp.com",
-//   // For databases not in the us-central1 location, databaseURL will be of the
-//   // form https://[databaseName].[region].firebasedatabase.app.
-//   // For example, https://your-database-123.europe-west1.firebasedatabase.app
-//   databaseURL: "https://databaseName.firebaseio.com",
-//   storageBucket: "bucket.appspot.com",
-// };
-
-// const app = initializeApp(firebaseConfig);
-
-// // Get a reference to the database service
-// export const database = getDatabase(app);
 
 interface IGlobalContext {
   accessToken?: string;
@@ -83,7 +67,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   });
 
   const uploadLink = createUploadLink({
-    uri: "http://backend04.codebootcamp.co.kr/graphql",
+    uri: "https://backend04.codebootcamp.co.kr/graphql",
     headers: {
       authorization: `Bearer ${accessToken}`,
     },
